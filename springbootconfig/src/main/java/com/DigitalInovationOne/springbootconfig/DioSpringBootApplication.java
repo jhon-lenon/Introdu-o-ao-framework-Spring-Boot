be6a2d20661ebb22fc679ddd45d0b991f6a8eb1c
jhon-lenon <jhonlenondslb@gmail.com>
2023-01-14 12:18:13 -0300

    projeto

---
+++ springbootconfig/src/main/java/com/DigitalInovationOne/springbootconfig/DioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.DigitalInovationOne.springbootconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringBootApplication.class, args);
+	}
+
+}
---
+++ springbootconfig/src/test/java/com/DigitalInovationOne/springbootconfig/DioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DigitalInovationOne.springbootconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
